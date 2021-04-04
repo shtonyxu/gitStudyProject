247261223b0490987848db6a5a024d71db248400
shtonyxu <shtonyxu@gmail.com>
2021-04-05 01:43:31 +0800

    This is committed in 2.0 by Tony

--- main.go
+++ main.go
@@ -5,4 +5,13 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, world!")
 	fmt.Println("This is 2.0 branch")
+	fmt.Println("This is 2.0 branch")
+	fmt.Println("This is 2.0 branch")
+	fmt.Println("This is 2.0 branch")
+	fmt.Println("This is 2.0 branch")
+	fmt.Println("This is 2.0 branch")
+	fmt.Println("This is 2.0 branch")
+	fmt.Println("This is 2.0 branch")
+	fmt.Println("This is 2.0 branch")
+
 }
